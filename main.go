8590f60f692d3f66fde389d20f745f0567689142
dheeraj.singh <dheeraj.singh@bytedance.com>
2025-02-02 22:20:48 +0800

    fix: add client code

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dheeraj-sn/design-patterns/abstractfactory/furniturefactory"
 )
 
 func main() {
+	abstractFactory()
+
+}
+
+func abstractFactory() {
 	mf := furniturefactory.NewModern()
 	mfc := mf.CreateChair()
 	mfs := mf.CreateSofa()
 	mfct := mf.CreateCoffeeTable()
 	mfc.HasLegs()
+	mfc.SitOn()
 	mfs.HasLegs()
 	mfct.HasLegs()
+	mfct.PlaceOn()
+
+	fmt.Println("============")
+	mf = furniturefactory.NewArtDeco()
+	mfc = mf.CreateChair()
+	mfs = mf.CreateSofa()
+	mfct = mf.CreateCoffeeTable()
+	mfc.HasLegs()
+	mfc.SitOn()
+	mfs.HasLegs()
+	mfct.HasLegs()
+	mfct.PlaceOn()
+	fmt.Println("============")
+	mf = furniturefactory.NewVictorian()
+	mfc = mf.CreateChair()
+	mfs = mf.CreateSofa()
+	mfct = mf.CreateCoffeeTable()
+	mfc.HasLegs()
+	mfc.SitOn()
+	mfs.HasLegs()
+	mfct.HasLegs()
+	mfct.PlaceOn()
 }
